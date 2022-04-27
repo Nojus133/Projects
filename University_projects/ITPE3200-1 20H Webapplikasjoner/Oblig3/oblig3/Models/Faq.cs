83f53ae8718382c477d41f47171477f2ccc9c1a9
Nojus <54367157+Nojus133@users.noreply.github.com>
2022-04-27 11:27:18 +0200

    Add files via upload

---
+++ University_projects/ITPE3200-1 20H Webapplikasjoner/Oblig3/oblig3/Models/Faq.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oblig3.Models
+{
+    public class Faq
+    {
+        public int Id { get; set; }
+        
+        public string Spors { get; set; }
+        public string Svar { get; set; }
+        public int Rating { get; set; }
+        public string Kategori { get; set; }
+    }
+}
